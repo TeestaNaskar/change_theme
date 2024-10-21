4a52ad417ea0c705a1fef3428db7f6997cc58b6d
Teesta Naskar <63419282+TeestaNaskar@users.noreply.github.com>
2024-10-21 12:30:00 -0400

    Create change.theme.R

---
+++ change.theme.R
@@ -0,0 +1,6 @@
+theme_set(theme_classic(base_size = 20) +
+            theme(
+              axis.title.y = element_text(face = "bold", margin = margin(0,20,0,0), size = rel(1.1), color = 'black'),
+              axis.title.x = element_text(hjust = 0.5, face = "bold", margin = margin(20,0,0,0), size = rel(1.1), color = 'black'),
+              plot.title = element_text(hjust = 0.5)
+            ))
